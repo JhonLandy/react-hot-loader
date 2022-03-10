5903ebf5a39206a12735e9a0c4211d479355ea38
YuanChengLang <709364178@qq.com>
2022-03-10 13:48:53 +0800

    fix:修复@obseve导致不能热梗问题

--- inject/index.js
+++ inject/index.js
@@ -1,27 +1,27 @@
-const R = require('ramda')
+const R = require("ramda");
 
 const templateOptions = {
     placeholderPattern: /^([A-Z0-9]+)([A-Z0-9_]+)$/,
 };
-const fail = []
-const success = []
-function injectCodeForRoot (api, state, root) {
-    if (!root)return
-    const { template } = api
+const fail = [];
+const success = [];
+function injectCodeForRoot(api, state, root) {
+    if (!root) return;
+    const { template } = api;
     const renderHotCode = `
         (function () {  
             if (module && module.hot) {
                 module.hot.accept()
             }
         })(this);
-    `
-    const rootAst = template(renderHotCode, templateOptions)()
-    root.body.push(rootAst)
+    `;
+    const rootAst = template(renderHotCode, templateOptions)();
+    root.body.push(rootAst);
 }
-function injectCodeForFunction (api, state, node) {
-    if (!node)return
-    const { template, types } = api
-    const { depends, reactNode } = state
+function injectCodeForFunction(api, state, node) {
+    if (!node) return;
+    const { template, types } = api;
+    const { depends, reactNode } = state;
     const functionHotCode = `
         (function () {  
            const [_, forceUpdate] = useReducer(x => x + 1, 0)
@@ -31,37 +31,40 @@ function injectCodeForFunction (api, state, node) {
                 })
             }
         })();
-    `
-    const newNode = types.importSpecifier(types.identifier('useReducer'), types.identifier('useReducer'))
+    `;
+    const newNode = types.importSpecifier(
+        types.identifier("useReducer"),
+        types.identifier("useReducer")
+    );
     const hasReducer = reactNode.specifiers.find(s => {
         if (!s.imported) {
-            return false
+            return false;
         } else {
-            return s.imported.name === newNode.imported.name
+            return s.imported.name === newNode.imported.name;
         }
-    })
+    });
     if (!hasReducer) {
-        reactNode.specifiers.push(newNode)
+        reactNode.specifiers.push(newNode);
     }
-    const fnAst = template(functionHotCode, templateOptions)({
-        DEPS: depends
-    })
-    const cBody = node.body
+    const fnAst = template(
+        functionHotCode,
+        templateOptions
+    )({
+        DEPS: depends,
+    });
+    const cBody = node.body;
     if (types.isBlockStatement(cBody)) {
-        node.body.body.unshift(fnAst)
+        node.body.body.unshift(fnAst);
     } else {
-        const returnStatement = types.returnStatement(cBody)
-        const blockStatement = types.blockStatement([
-            fnAst,
-            returnStatement
-        ])
-        node.body = blockStatement
+        const returnStatement = types.returnStatement(cBody);
+        const blockStatement = types.blockStatement([fnAst, returnStatement]);
+        node.body = blockStatement;
     }
 }
-function injectCodeForClass (api, state, node) {
-    if (!node) return
-    const { template, types } = api
-    const { depends } = state
+function injectCodeForClass(api, state, node) {
+    if (!node) return;
+    const { template, types } = api;
+    const { depends } = state;
     const classHotCode = `
         (function () {  
             if (module && module.hot) {
@@ -73,37 +76,37 @@ function injectCodeForClass (api, state, node) {
                 })
             }
         }).call(this);
-    `
-    const classAst = template(classHotCode, templateOptions)({
-        DEPS: depends
-    })
+    `;
+    const classAst = template(
+        classHotCode,
+        templateOptions
+    )({
+        DEPS: depends,
+    });
     const superExpression = types.expressionStatement(
-        types.callExpression(types.super(), [types.identifier('props')])
-    )
-    const blockStatement = types.blockStatement([
-        superExpression,
-        classAst
-    ])
+        types.callExpression(types.super(), [types.identifier("props")])
+    );
+    const blockStatement = types.blockStatement([superExpression, classAst]);
     const constructorMethodNode = types.classMethod(
-        'constructor',
-        types.identifier('constructor'),
-        [types.identifier('props')],
+        "constructor",
+        types.identifier("constructor"),
+        [types.identifier("props")],
         blockStatement
-    )
+    );
     const constructorMethod = node.body.body.find(method => {
-        return method.kind === 'constructor'
-    })
+        return method.kind === "constructor";
+    });
 
     if (R.isNil(constructorMethod)) {
-        node.body.body.unshift(constructorMethodNode)
+        node.body.body.unshift(constructorMethodNode);
     } else {
-        constructorMethod.body.body.push(classAst)
+        constructorMethod.body.body.push(classAst);
     }
 }
 function injectCodeForSpecialClass(api, state, node) {
-    if (!node)return
-    const { template, types } = api
-    const { depends } = state
+    if (!node) return;
+    const { template, types } = api;
+    const { depends } = state;
     const classHotCode = `
         (function () {  
             if (module && module.hot) {
@@ -115,40 +118,47 @@ function injectCodeForSpecialClass(api, state, node) {
                 })
             }
         })()
-    `
-    const classAst = template(classHotCode, templateOptions)({
-        DEPS: depends
-    })
-    const cBody = node.body
+    `;
+    const classAst = template(
+        classHotCode,
+        templateOptions
+    )({
+        DEPS: depends,
+    });
+    const cBody = node.body;
     if (types.isBlockStatement(cBody)) {
-        const returnStatement = node.body.body.pop()
-        node.body.body.push(classAst, returnStatement)
-    } 
+        const returnStatement = node.body.body.pop();
+        node.body.body.push(classAst, returnStatement);
+    }
 }
 function injectCodeForOtherType(api, state, node) {
-    const { depends, exportNode } = state
-    const { template } = api
+    const { depends, exportNode } = state;
+    const { template } = api;
     const buildTagger = template(
         `reactHotRegister 
                     ? reactHotRegister(function(update) {
                         if (module && module.hot) {
                             module.hot.accept(DEPENDENCIES, function() {
+                                window.isHot = true
                                 update()
+                                window.isHot = false
                             })
                         }
                     })(COMPONENT) 
                     : COMPONENT
-                `, templateOptions)({
+                `,
+        templateOptions
+    )({
         DEPENDENCIES: depends,
-        COMPONENT: node
-    })
-    exportNode.declaration = buildTagger
+        COMPONENT: node,
+    });
+    exportNode.declaration = buildTagger;
 }
 function injectCodeByType(api, state, node, fnCaller) {
     if (!node) {
-        return
+        return;
     }
-    const { types, template } = api
+    const { types } = api;
     const {
         isArrayExpression,
         isObjectExpression,
@@ -158,32 +168,32 @@ function injectCodeByType(api, state, node, fnCaller) {
         isClassExpression,
         isClassDeclaration,
         isCallExpression,
-        isIdentifier
-    } = types
-   
+        isIdentifier,
+    } = types;
+
     switch (true) {
         case isArrayExpression(node):
         case isObjectExpression(node):
-            return
+            return;
         case isArrowFunctionExpression(node):
         case isFunctionExpression(node):
         case isFunctionDeclaration(node): {
-            injectCodeForFunction(api, state, node)
-            success.push(state.filename)
-            break
+            injectCodeForFunction(api, state, node);
+            success.push(state.filename);
+            break;
         }
         case isClassExpression(node):
         case isClassDeclaration(node): {
-            injectCodeForClass(api, state, node)
-            success.push(state.filename)
-            break
+            injectCodeForClass(api, state, node);
+            success.push(state.filename);
+            break;
         }
         case isIdentifier(node):
         case isCallExpression(node):
-            injectCodeForOtherType(api, state, node)
-            break
+            injectCodeForOtherType(api, state, node);
+            break;
         default:
-            fail.push(state.filename)
+            fail.push(state.filename);
     }
 }
 
@@ -192,5 +202,5 @@ module.exports = {
     injectCodeForFunction,
     injectCodeForClass,
     injectCodeForSpecialClass,
-    injectCodeByType
-}
+    injectCodeByType,
+};
--- patch/index.js
+++ patch/index.js
@@ -1,19 +1,39 @@
-import React, { useReducer, forwardRef } from 'react'
-import * as R from 'ramda'
-
+import React, { useReducer, forwardRef, useRef, useImperativeHandle } from "react";
+import * as R from "ramda";
+window.isHot = false;
 window.reactHotRegister = function (callback) {
     return Comp => {
-        if (typeof Comp === "function"
-            && R.includes("use", Comp.name)// 不处理自定义hooks
+        if (
+            typeof Comp === "function" &&
+            R.includes("use", Comp.name) // 不处理自定义hooks
         ) {
-            return Comp
+            return Comp;
         }
         return forwardRef((props, ref) => {
-            const [_, forceUpdate] = useReducer(x => x + 1, 0)
-            callback && callback(forceUpdate)
-            return <Comp {...props} ref={ref}/>
-        })
-    }
-}
+            const _ref = useRef();
+            const reducer = useReducer(x => x + 1, 0);
 
-export {}
+            if (Comp.compare) {
+                const compare = Comp.compare;
+                Comp.compare = (...state) => (isHot ? false : compare(...state));
+            } else {
+                const prototype = Comp.prototype;
+                const shouldComponentUpdate = prototype?.shouldComponentUpdate;
+
+                if (prototype && shouldComponentUpdate) {
+                    if (!_ref.current) {
+                        _ref.current = new Comp(props);
+                    }
+                    const instance = _ref.current;
+                    prototype.shouldComponentUpdate = (...state) =>
+                        isHot ? true : shouldComponentUpdate.call(instance, ...state);
+                }
+            }
+            useImperativeHandle(ref, () => _ref.current, []);
+            callback(reducer[1]);
+            return <Comp {...props} ref={_ref} />;
+        });
+    };
+};
+
+export {};
